925446e6e9b7a23128b8a379dadd4be006efb073
Joyce <joyce@industrialnext.ai>
2024-05-10 10:57:51 -0400

    updated requirments

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Scraping a dynamic website with Selenium to extract WaterlooWork job posted information.
+
+To Use:
+git clone repo
+pip install -r requirements.txt 
+change appsettings.json to make your WW username and password
--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
 
 logging.getLogger().setLevel(logging.INFO)
